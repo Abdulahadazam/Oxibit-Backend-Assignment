16b83a3d76878cab7b48bac85559dcd5b2524670
Abdulahad <abdulahadazam607@gmail.com>
2024-11-28 00:36:40 +0500

    hellio

--- test_project/settings.py
+++ test_project/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g$v91&6-s$m2&=%-ho!)xz(vi8algflm4oeumgyrw4s$ji%$a#'
+# SECRET_KEY = 'django-insecure-g$v91&6-s$m2&=%-ho!)xz(vi8algflm4oeumgyrw4s$ji%$a#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
